bc7ba93b897df9f82a5acf532a55745247a4e0e9
apelone <apelonegames@gmail.com>
2023-03-28 18:20:04 -0300

    Fist commit

---
+++ src/br/com/dio/model/NovoProjeto.java
@@ -0,0 +1,10 @@
+package br.com.dio.model;
+
+public class NovoProjeto {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        System.out.println("Hello Wolrd!"+(a+b));
+
+    }
+}
